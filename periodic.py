1698658ebc14e83ac7491f05256a24331a7a992d
Navash914 <naveedashfaq914@gmail.com>
2021-01-31 15:04:23 -0500

    Added scheduler to run every 5 mins

---
+++ periodic.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+class PeriodicExecutor(threading.Thread):
+
+    def __init__(self, sleep, func, *params):
+        'Execute func(params) every "sleep" seconds'
+        self.func = func
+        self.params = params
+        self.sleep = sleep
+        threading.Thread.__init__(self, name = "PeriodicExecutor")
+        self.setDaemon(True)
+
+    def run(self):
+        while True:
+            if self.func is None:
+                sys.exit(0)
+            self.func(*self.params)
+            time.sleep(self.sleep)
--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import api_calls
 import sys
 import consts
 import discord_bot
+from periodic import PeriodicExecutor
 
 needsSave = False
 
@@ -15,6 +16,7 @@ def requestDataSave():
 def saveSummonerData():
     global needsSave
     needsSave = False
+    print("Saving new summoner data")
     data.saveSummonerData()
 
 def querySummonerCurrentRank(summoner):
@@ -59,10 +61,9 @@ def querySummonerCurrentGame(summoner):
             requestDataSave()
 
 
-def run():
-    summonerData = data.loadSummonerData()
-
+def run(summonerData):
     # Need to run this loop every 5 mins
+    print("Querying Data For Summoners")
     for summonerName in summonerData:
         summoner = summonerData[summonerName]
         querySummonerCurrentGame(summoner)
@@ -89,4 +90,7 @@ if __name__ == "__main__":
             print("Invalid arguments")
             printHelp()
             sys.exit()
-    run()
+    summonerData = data.loadSummonerData()
+    FIVE_MINUTES = 5 * 60
+    run_thread = PeriodicExecutor(FIVE_MINUTES, run, summonerData)
+    run_thread.run()

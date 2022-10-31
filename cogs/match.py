3559d77c0ecd2fbf83621a6164c9bbdf6f6b1c1d
Sarhad Salam <sarhadmaisoon18@gmail.com>
2022-10-31 00:42:01 +0000

    Fixed browser sync issue

--- cogs/match.py
+++ cogs/match.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import image_handler
 import discord
+import image_handler
 
 class Match(commands.Cog):
     def __init__(self, bot):
@@ -10,11 +10,14 @@ class Match(commands.Cog):
     # Commands
     @commands.command()
     async def match(self, ctx, gameId):
+        await ctx.send(f"Obtaining image for game ID {gameId}")
+
         image_handler.get_image(gameId)
         image_handler.crop_image_post_game(gameId)
 
-        await ctx.send(file=discord.File(f"cropped_{gameId}.png"))
+        await ctx.send(f"image obtained for game ID {gameId}")
 
+        await ctx.send(file=discord.File(f"cropped_{gameId}.png"))
         image_handler.remove_images(gameId)
 
 # Connect cog to bot
--- image_handler.py
+++ image_handler.py
@@ -3,45 +3,45 @@ from api_calls import call_api
 import json
 from PIL import Image
 import os
+import sys
 
 import settings
-
-if settings.ENABLE_SCREENSHOT:
-    from pyvirtualdisplay import Display
 
 from selenium import webdriver
 
 
 def get_image(game_id):
-    # start displayca    if settings.ENABLE_SCREENSHOT:
-    vdisplay = Display(visible=0, size=(2560, 1440))
-    vdisplay.start()
-
+    # print("Getting image")
     # start browser
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
-    selenium = webdriver.Chrome()
+    options.add_argument("start-maximized"); 
+    options.add_argument("enable-automation"); 
+    options.add_argument("--disable-browser-side-navigation");
+    options.add_argument("--disable-gpu");
+    selenium = webdriver.Chrome(options=options)
+    selenium.set_window_size(1280, 950)
     selenium.maximize_window()
 
+    # print("Have browser")
     selenium.get(f'https://www.leagueofgraphs.com/match/na/{game_id}')
+    # print("Send request")
+
     selenium.save_screenshot(f"screenshot_{game_id}.png")
+    # print("Save")
 
     selenium.quit()
-
-    if settings.ENABLE_SCREENSHOT:
-        vdisplay.stop()
-
 
 def crop_image_post_game(game_id):
     img = Image.open(f"screenshot_{game_id}.png")
 
     # Setting the points for cropped image
     top = 380
-    left = 320
-    right = 1380
-    bottom = 920
+    left = 250
+    right = 1250
+    bottom = 950
 
     img.crop((left, top, right, bottom)).save(f"cropped_{game_id}.png")
 
@@ -55,6 +55,6 @@ def remove_images(game_id):
 
 
 if __name__ == "__main__":
-    game_id = "4046823128"
-    # get_image(game_id)
+    game_id = sys.argv[1]
+    get_image(game_id)
     crop_image_post_game(game_id)
